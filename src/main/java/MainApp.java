8546c7eb2b0732ddd566ce429df9591727bbac28
nikm31 <86549082+nikm31@users.noreply.github.com>
2021-07-22 09:50:46 +0300

    1 - вынес тесты в новый класс и не статический метод 2 - добавил создание экземпляра класса на основе переданного

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -3,48 +3,12 @@ import java.util.*;
 
 public class MainApp {
     public static void main(String[] args) {
-/*
-        тут так и не разобрался как рефлексией создать экземпляр класса и использовать его в invoke
-        так же пробовал реализовать через HashMap, где ключ преоритет
-*/
-        MethodsForTests test = new MethodsForTests();
-        Class testClass = MethodsForTests.class;
-        Method[] allMethods = testClass.getDeclaredMethods();
-        List<Method> methodsTest = new ArrayList<>();
-        short as = 0;
-        short bs = 0;
-        Method before = null;
-        Method after = null;
-        for (Method method : allMethods) {
-            if (method.getAnnotation(Test.class) != null) {
-                methodsTest.add(method);
-            }
-            if (method.getAnnotation(BeforeSuite.class) != null) {
-                bs++;
-                if (bs > 1) {
-                    throw new RuntimeException("В ваших методах более одной аннотации BeforeSuite");
-                }
-                before = method;
-            }
-            if (method.getAnnotation(AfterSuite.class) != null) {
-                as++;
-                if (as > 1) {
-                    throw new RuntimeException("В ваших методах более одной аннотации BeforeSuite");
-                }
-                after = method;
-            }
-        }
-
-        methodsTest.sort(Comparator.comparingInt(obj -> obj.getAnnotation(Test.class).value()));
-
-        try {
-            before.invoke(test);
-            for (Method method : methodsTest) {
-                method.invoke(test);
-            }
-            after.invoke(test);
-        } catch (InvocationTargetException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        MethodsForTests tests = new MethodsForTests();
+        StartTesting startTesting = new StartTesting();
+        startTesting.start(tests.getClass());
     }
+
+
+
 }
+
